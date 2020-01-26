773cfbfb9c245f92522b9ff4221f3530cb172727
fangshixiang <fangshixiang@vipkid.com.cn>
2020-01-26 19:12:49 +0800

    Spring环境的序列化示例

--- spring-jackson/src/main/java/com/yourbatman/spring/config/WebMvcConfig.java
+++ spring-jackson/src/main/java/com/yourbatman/spring/config/WebMvcConfig.java
@@ -1,15 +1,20 @@
 package com.yourbatman.spring.config;
 
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * web子容器初始化的配置
@@ -24,4 +29,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.forEach(c -> {
+            Class<AbstractJackson2HttpMessageConverter> jacksonClazz = AbstractJackson2HttpMessageConverter.class;
+            if (jacksonClazz.isAssignableFrom(c.getClass())) {
+                AbstractJackson2HttpMessageConverter jacksonConverter = jacksonClazz.cast(c);
+                jacksonConverter.getObjectMapper().disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            }
+        });
+    }
 }
--- spring-jackson/src/main/java/com/yourbatman/spring/controller/DemoController.java
+++ spring-jackson/src/main/java/com/yourbatman/spring/controller/DemoController.java
@@ -1,18 +1,54 @@
 package com.yourbatman.spring.controller;
 
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("/get/time")
+    @GetMapping("/get")
     public Object get() {
-        return "hello world";
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("date", new Date());
+        map.put("timestamp", new Timestamp(System.currentTimeMillis()));
+        map.put("localDateTime", LocalDateTime.now());
+        map.put("localDate", LocalDate.now());
+        map.put("localTime", LocalTime.now());
+        map.put("instant", Instant.now());
+        return map;
     }
 
 
+    @PostMapping("/post")
+    public Object post(@RequestBody Demo demo) {
+        System.out.println(demo);
+        return "success";
+    }
+
+    @Setter
+    @ToString
+    private static class Demo {
+        private Date date;
+        // private Timestamp timestamp; // 它没有空构造，所以默认不能被反序列化
+        private LocalDateTime localDateTime;
+        private LocalDate localDate;
+        private LocalTime localTime;
+        private Instant instant;
+    }
 }
