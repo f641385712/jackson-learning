6dc7b03a4868de083b639e31c9dca957aedc26c0
fangshixiang <fangshixiang@vipkid.com.cn>
2020-02-06 22:12:09 +0800

    Jackson和FastJson

---
+++ jacksonandfastjson/src/main/java/com/yourbatman/jacksonandfastjson/TestCase.java
@@ -0,0 +1,54 @@
+package com.yourbatman.jacksonandfastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCase {
+
+    private static final int MAX = 3000_0000;
+    private static final List<Integer> OBJS = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < MAX; i++) {
+            OBJS.add(i);
+        }
+    }
+
+    @Test
+    public void fun1() {
+        System.out.println("MAX为：" + MAX);
+        ObjectMapper mapper = new ObjectMapper();
+
+        run("fastjson", () -> JSON.toJSONString(OBJS));
+        run("jackson", () -> jacksonSer(mapper, OBJS));
+    }
+
+
+    private static void jacksonSer(ObjectMapper mapper, Object obj) {
+        try {
+            mapper.writeValueAsString(OBJS);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fastjsonSer(Object obj) {
+        JSON.toJSONString(OBJS);
+    }
+
+    // 这里仅是把Runnable当作@FunctionalInterface来使而已，并不是启线程
+    private static void run(String name, Runnable task) {
+        Instant start = Instant.now();
+        task.run();
+        Instant end = Instant.now();
+
+        System.out.printf("【%s】耗时： %s 毫秒\n", name, Duration.between(start, end).toMillis());
+    }
+}
